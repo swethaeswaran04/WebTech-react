f48cdf6c3330ccefc0e94d9b0e8bc3eeb65f9797
swethaeswaran04 <150774265+swethaeswaran04@users.noreply.github.com>
2024-09-02 08:55:43 +0530

    Create CY.js

---
+++ day_2/CY.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Cyday2 ()
+{
+    return (
+        <div style={{color:"Blue",backgroundColor:"purple",textAlign:"center"}}>
+            
+            <p style={{textAlign:"center"}}>It is a functional component</p>
+            <p style={{textAlign:"center"}}> 
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb6gOxvbxL7xHqj6nZT_4PqOkt0r_2ri-yow&s"></img>
+            </p>     
+            </div>
+    )
+}
+export default Cyday2;
